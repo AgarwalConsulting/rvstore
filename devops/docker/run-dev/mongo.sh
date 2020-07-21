9e9610062bc1bf98d45dd96d6c63580e82d2f9cf
Gaurav Agarwal <algogrit@gmail.com>
2020-07-21 20:26:25 +0530

    Mounting volume for mongoDB.

--- devops/docker/run-dev/mongo.sh
+++ devops/docker/run-dev/mongo.sh
@@ -3,6 +3,7 @@
 docker run --rm -it -p 27017:27017 \
   -e MONGO_INITDB_ROOT_USERNAME=root \
   -e MONGO_INITDB_ROOT_PASSWORD=example \
+  -v `pwd`/project-resources/mongo/data:/data/db
   mongo
 
 # docker run --rm -it -p 8081:8081 \
